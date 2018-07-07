6c6e4073fecea8158e6244fdd80f530e7f1729a0
Daniel Peña <digrape07@gmail.com>
2018-07-06 20:18:50 -0400

    Init

---
+++ SIBACME/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SIBACME.Models;
+
+namespace SIBACME.Controllers
+{
+    public class HomeController : Controller
+    {
+        DummyData _data;
+        public HomeController()
+        {
+            _data = new DummyData();
+        }
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        /**
+         * RESERVA DE RECURSO BIBLIOGRAFICO  - Daniel
+         * */
+        [HttpGet]
+        public IActionResult Reserva()
+        {
+
+            return View();
+        }
+        [HttpPut]
+        public IActionResult Reserva(Book book)
+        {
+            Random rdn = new Random();
+            if(book == null) // Testing purporse
+            {
+                book = _data.Books.ElementAt(rdn.Next(0, _data.Books.Count));
+            }
+            return View();
+        }
+
+        /**
+         * PRESTAMO DE RECURSO BIBLIOGRAFICO - Dionis
+         * */
+        [HttpGet]
+        public IActionResult Prestamo()
+        {
+            return View();
+        }
+        [HttpPut]
+        public IActionResult Prestamo(Book book)
+        {
+            return View();
+        }
+
+        /**
+          * DEVUELTA DE RECURSO BIBLIOGRAFICO - Daniel, Dionis, Pedro
+          * */
+        [HttpGet]
+        public IActionResult Devuelta()
+        {
+            return View();
+        }
+        [HttpPut]
+        public IActionResult Devuelta(Book book)
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SIBACME/Models/DummyData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SIBACME.Models;
+
+namespace SIBACME.Models
+{
+    public class DummyData
+    {
+        public ICollection<Book> Books {
+
+            get { return Books; }
+
+            set {
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "Los mejores cuentos", BookAuthor = "Anton Chejov" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "Una hormiga gigante", BookAuthor = "Daniel Peña" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+                Books.Add(new Book { BookId = 1, BookTitle = "La casa viviente", BookAuthor = "Marco Polo" });
+            }
+        }
+    }
+}
